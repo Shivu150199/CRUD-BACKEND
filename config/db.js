680cb61005f1eff8be4d0546ea5c1198ffc9c6e3
Shivam Singh gautam <105829038+Shivu150199@users.noreply.github.com>
2023-07-12 00:28:55 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const mysql = require('mysql2')
 const urlDB = `mysql://root:26L5wOo5efVizm4dMNuF@containers-us-west-181.railway.app:7221/railway`
 
-const db = mysql.createConnection(
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_DATABASE,
-  urlDB
+const db = mysql.createPool(
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  
 )
 
 module.exports=db;
